a5e5615bef4018044b41574334fce08a3e1087fb
Donny Airlangga <donny.airlangga@gmail.com>
2020-04-09 16:55:57 +0700

    yes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+<b-container fluid>
+ <b-navbar toggleable="lg" type="dark" variant="info">
+  <b-navbar-brand href="#">Demo Vue</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/about">About</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>  
+
+ </b-navbar>      
+  
+
+  <b-row>
+     <b-col>
+       <b-nav vertical class="w-75">
+        <b-nav-item active>Active</b-nav-item>
+        <b-nav-item>Login</b-nav-item>
+        <b-nav-item>List</b-nav-item>
+        <b-nav-item disabled>Disabled</b-nav-item>
+      </b-nav>
+     </b-col>
+     <b-col cols="10"><router-view/></b-col>
+    
+  </b-row>
+</b-container>
+ 
+  
+ 
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="page-content">
+    <b-card title="Please enter">
+      <b-form @submit="onSubmit" @reset="onReset" >
+         <b-form-group
+        id="input-group-1"
+        label="Username:"
+        label-for="input-1"
+        description="Your username."
+       >
+        <b-form-input
+          id="input-1"
+          v-model="user.username"
+          type="text"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+        <b-form-group
+        id="input-group-1"
+        label="Password:"
+        label-for="input-1"
+        description="Your username."
+       >
+        <b-form-input
+          id="input-1"
+          v-model="user.password"
+          type="password"
+          required
+          placeholder="Enter Password"
+        ></b-form-input>
+           <b-button type="submit" variant="primary">Submit</b-button>
+            &nbsp;
+            <b-button type="reset" variant="danger">Reset</b-button>
+            <b-button type="button" variant="secondary" @click="onClickRead">Read</b-button>
+      </b-form-group>
+      </b-form>    
+ 
+   
+  </b-card>
+
+  <p>&nbsp;</p>
+   <div>
+    <b-table striped hover :items="users"></b-table>
+  </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: null,
+      user: {
+        username: '',
+        password: '',
+      },
+    }
+  },
+  
+  methods: {
+    onClickRead() {
+
+
+      axios
+        .get('http://localhost:3000/users')
+        .then(response => {
+          this.users = response.data
+          console.log(this.users)
+        })
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
+
+
+    },
+    onSubmit(evt) {
+      evt.preventDefault()
+      console.log(JSON.stringify(this.user))
+      axios.post('http://localhost:3000/users', this.user)
+
+    },
+    onReset(evt) {
+      evt.preventDefault()
+      // Reset our form values
+      this.user.username = ''
+      this.user.password = ''
+      this.$nextTick(() => {
+        // this.show = true
+      })
+    }
+  }
+}
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+/*
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+*/
+</script>
